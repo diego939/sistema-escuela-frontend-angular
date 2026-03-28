0d33db349be4322b7d4930a4081c2fa1bbf2266d
diego939 <diegodavidalmiron1990@gmail.com>
2026-03-28 11:05:16 -0300

    fix add nav component

---
+++ src/app/layout/navbar/navbar.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { LayoutService } from '../../core/services/layout.service';
+import { AuthService } from '../../core/services/auth.service';
+import { RouterModule, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [RouterModule, CommonModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+constructor(
+  private layoutService: LayoutService,
+  private authService: AuthService,
+  private router: Router
+) {}
+
+isUserMenuOpen = false;
+
+emailUsuario = this.authService.obtenerUsuario()?.email || 'Usuario';
+
+ngOnInit() {
+  this.layoutService.userMenuOpen$.subscribe(v => this.isUserMenuOpen = v);
+}
+
+toggleSidebar() {
+  this.layoutService.toggleSidebar();
+}
+
+toggleUserMenu() {
+  this.layoutService.toggleUserMenu();
+}
+
+logout() {
+  this.toggleSidebar(); // Cerrar el sidebar al hacer logout
+  this.toggleUserMenu(); // Cerrar el menú de usuario al hacer logout
+  this.authService.logout();
+  this.router.navigate(['/login']);
+}
+}
