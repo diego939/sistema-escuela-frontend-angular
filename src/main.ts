5dfc2ed90fff5557cdd3e20676430c7b64f21acf
diego939 <diegodavidalmiron1990@gmail.com>
2026-03-29 22:07:39 -0300

    fix add withFetch

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,13 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes'; // 🔥 IMPORTANTE: Asegúrate de importar tus rutas correctamente
-import { provideHttpClient } from '@angular/common/http'; // 🔥 IMPORTANTE
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient() // 🔥 ESTO SOLUCIONA TODO
+    provideHttpClient(withFetch())
   ]
 };
 
