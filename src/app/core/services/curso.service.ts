1ef9bf1d79bcab26b7888a4da1ad02535998c1d4
diego939 <diegodavidalmiron1990@gmail.com>
2026-03-30 20:01:51 -0300

    fix create course service

---
+++ src/app/core/services/curso.service.ts
@@ -0,0 +1,45 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import {
+  CursoCrearRequest,
+  CursoPaginadoResponse
+} from '../models/curso.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CursoService {
+  private apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  getCursosPaginados(
+    page: number,
+    pageSize: number,
+    search: string,
+    sortBy: string,
+    sortDescending: boolean
+  ): Observable<CursoPaginadoResponse> {
+
+    const params = new HttpParams()
+      .set('pageNumber', page)
+      .set('pageSize', pageSize)
+      .set('search', search)
+      .set('sortBy', sortBy)
+      .set('sortDescending', sortDescending.toString());
+
+    return this.http.get<CursoPaginadoResponse>(
+      `${this.apiUrl}/api/Curso/lista-paginado`,
+      { params }
+    );
+  }
+
+  crear(body: CursoCrearRequest): Observable<unknown> {
+    return this.http.post(
+      `${this.apiUrl}/api/Curso/crear`,
+      body
+    );
+  }
+}
