ef4264c03e30d8218ff7b25994af4555142cb45c
diego939 <diegodavidalmiron1990@gmail.com>
2026-03-30 20:01:26 -0300

    fix create interface course

---
+++ src/app/core/models/curso.model.ts
@@ -0,0 +1,29 @@
+export interface Curso {
+  id: number;
+  modulo: number;
+  division: string;
+  modalidad: string;
+  turno: string;
+  anio: number;
+  cupoMaximo: number;
+  fechaCreacion: Date | string;
+}
+
+export interface CursoPaginadoResponse {
+  data: Curso[];
+  totalRecords: number;
+  totalPages: number;
+  pageNumber: number;
+  pageSize: number;
+  hasPreviousPage: boolean;
+  hasNextPage: boolean;
+}
+
+export interface CursoCrearRequest {
+  modulo: number;
+  division: string;
+  modalidad: string;
+  turno: string;
+  anio: number;
+  cupoMaximo: number;
+}
