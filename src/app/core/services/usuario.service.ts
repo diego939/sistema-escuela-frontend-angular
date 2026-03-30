b14bab1fa766652ab7b1852f3dde0746f39ba418
diego939 <diegodavidalmiron1990@gmail.com>
2026-03-29 22:09:53 -0300

    fix add services in crud user

--- src/app/core/services/usuario.service.ts
+++ src/app/core/services/usuario.service.ts
@@ -2,7 +2,13 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
-import { UsuarioPaginadoResponse } from '../models/usuario.model';
+import {
+  Rol,
+  UsuarioActivarDesactivarRequest,
+  UsuarioCrearRequest,
+  UsuarioEditarRequest,
+  UsuarioPaginadoResponse
+} from '../models/usuario.model';
 
 @Injectable({
   providedIn: 'root'
@@ -24,5 +30,25 @@ export class UsuarioService {
     const response = this.http.get<UsuarioPaginadoResponse>(`${this.baseUrl}/api/Usuario/lista-paginado`, { params });
     console.log('Respuesta del servicio:', response);
     return response;
+  }
+
+  crear(body: UsuarioCrearRequest): Observable<unknown> {
+    return this.http.post<unknown>(`${this.baseUrl}/api/Usuario/crear`, body);
+  }
+
+  editar(body: UsuarioEditarRequest): Observable<unknown> {
+    return this.http.put<unknown>(`${this.baseUrl}/api/Usuario/editar`, body);
+  }
+
+  activar(body: UsuarioActivarDesactivarRequest): Observable<unknown> {
+    return this.http.put<unknown>(`${this.baseUrl}/api/Usuario/activar`, body);
+  }
+
+  desactivar(body: UsuarioActivarDesactivarRequest): Observable<unknown> {
+    return this.http.put<unknown>(`${this.baseUrl}/api/Usuario/desactivar`, body);
+  }
+
+  listarRoles(): Observable<Rol[]> {
+    return this.http.get<Rol[]>(`${this.baseUrl}/api/Usuario/listar-roles`);
   }
 }
