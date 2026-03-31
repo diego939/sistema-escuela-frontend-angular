b3730b25940bf1e7338b4594b698a61c50aa62e5
diego939 <diegodavidalmiron1990@gmail.com>
2026-03-31 20:39:16 -0300

    fix add model subjects

--- src/app/core/models/materia.model.ts
+++ src/app/core/models/materia.model.ts
@@ -1,7 +1,36 @@
+export interface Materia {
+  id: number;
+  descripcion: string;
+  fechaCreacion: Date | string;
+}
+
+export interface MateriaPaginadoResponse {
+  data: Materia[];
+  totalRecords: number;
+  totalPages: number;
+  pageNumber: number;
+  pageSize: number;
+  hasPreviousPage: boolean;
+  hasNextPage: boolean;
+}
 export interface CursoMateria {
   id: number;
   idCurso: number;
   idMateria: number;
   materia: string;
   fechaCreacion: Date | string;
+}
+
+export interface MateriaCrearRequest {
+  descripcion: string;
+}
+
+export interface MateriaEditarRequest {
+  id: number;
+  descripcion: string;
+}
+
+export interface AsociarMateriaRequest {
+  idCurso: number;
+  idMateria: number;
 }
